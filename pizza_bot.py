d2759c2a8ffbbf30fb0f6a9567815cd5f115abc8
Abhishek Kavishetti <abhishek.kavishetti@gmail.com>
2025-01-13 22:26:46 +0530

    updated port info

--- pizza_bot.py
+++ pizza_bot.py
@@ -64,4 +64,6 @@ def handle_user_input(user_input):
         return "Sorry, I didn't understand. Try asking about pizza types or costs."
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
